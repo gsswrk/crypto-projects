9b34b2f61b8dff94f2a21bf54506eceb542f01ec
dan o <omgeewtfbbq@gmail.com>
2021-10-25 10:07:24 -0700

    Update scrape_etherscan_contract_holders.py

--- scrape_etherscan_contract_holders.py
+++ scrape_etherscan_contract_holders.py
@@ -5,16 +5,19 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import sys
 
-contract_address = input('Enter contract address: ')
+# pass contract address to pull all current token holders and output to file called "token_holders.txt"
+contract_address = sys.argv[1]
 driver = webdriver.Chrome()
+
 driver.get(
     'https://etherscan.io/token/' + contract_address + '#balances')
 WebDriverWait(driver, 10).until(
     EC.frame_to_be_available_and_switch_to_it((By.ID, "tokeholdersiframe")))
 elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located(
     (By.XPATH, '//*[@id="maintable"]/div[3]/table/tbody//tr/td[2]//a')))
-text_file = open("output.txt", "w")
+text_file = open("token_holders.txt", "w")
 for ele in elements:
     text = re.findall('[^=]*$', ele.get_attribute('href'))
     print(text[0])
